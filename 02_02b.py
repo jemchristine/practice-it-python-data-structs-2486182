91c0ce568f1ecc7c9909a427d71c31117d7eb871
Jem Rivera <133500647+jemchristine@users.noreply.github.com>
2023-06-07 11:20:19 +0000

    02.02 modified

--- 02_02b.py
+++ 02_02b.py
@@ -1,7 +1,16 @@
+from collections import deque
+
+def check_palindrome(word):
+    palindrome = deque(word)
+    while len(palindrome) > 1:
+        if palindrome.pop() != palindrome.popleft():
+            return False
+    return True
+
 
 def main():
-    #add code here
-    return
+    word = "tacocat"
+    print(check_palindrome(word))
 
 if __name__ == "__main__":
     main()
