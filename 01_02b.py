0cd4dd8f95215471845d458308cbc895ce15c258
Jem Rivera <133500647+jemchristine@users.noreply.github.com>
2023-06-07 10:55:40 +0000

    01.02 modified

--- 01_02b.py
+++ 01_02b.py
@@ -1,6 +1,8 @@
+from collections import Counter
+
 def get_common_products(products_purchased):
-    #code goes here
-    return
+    counter = Counter(products_purchased)
+    return counter.most_common(3)
 
 def main():
     products_purchased = ["DES005",
