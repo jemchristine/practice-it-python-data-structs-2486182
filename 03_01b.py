d2a308af23dc1f53772ba2e82a3ad548bfbecc84
Jem Rivera <133500647+jemchristine@users.noreply.github.com>
2023-06-07 11:52:26 +0000

    03.01 modified

--- 03_01b.py
+++ 03_01b.py
@@ -1,10 +1,19 @@
+from collections import namedtuple
+
+
+def can_take_order(driver, num_packages):
+    return driver.car_capacity >= num_packages
+
 
 def main():
-    #add code here
-    #create a driver with a name, car type, and car capacity
-    #an example you can use to practice: "Iris", "Toyota Prius", 7
-    #check if they can take a certain order, given their car's capacity.
+    Driver = namedtuple("driver", ["name", "car_type", "car_capacity"])
+    driver_one = Driver("Iris", "Toyota Prius", 7)
+    driver_two = Driver("Mickie", "Hyundai Tucson", 15)
+    driver_three = Driver("Witty", "Hummer", 26)
+    print(driver_one)
+    print(can_take_order(driver_three, 20))
     return
+
 
 if __name__ == "__main__":
     main()
